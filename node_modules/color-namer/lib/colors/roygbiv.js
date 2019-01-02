2e6486b9443b8edaae3e791a7ef0b3fccf637677
Ladislav Janeček <janek@brainz.cz>
2019-01-02 20:41:50 +0100

    Remove the now ignored directory node_modules

--- node_modules/color-namer/lib/colors/roygbiv.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-  { name: 'red', hex: '#FF0000' },
-  { name: 'orange', hex: '#FFA500' },
-  { name: 'yellow', hex: '#FFFF00' },
-  { name: 'green', hex: '#008000' },
-  { name: 'blue', hex: '#0000FF' },
-  { name: 'indigo', hex: '#4B0082' },
-  { name: 'violet', hex: '#EE82EE' }
-]
